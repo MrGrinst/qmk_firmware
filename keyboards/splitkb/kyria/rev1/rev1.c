5d0efe41a23cd6591845c29817a748469665cb50
Kyle Grinstead <kyleag@hey.com>
2021-12-06 11:13:18 -0700

    update rotary encoders

--- keyboards/splitkb/kyria/rev1/rev1.c
+++ keyboards/splitkb/kyria/rev1/rev1.c
@@ -137,16 +137,16 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_DOWN);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_UP);
         }
     }
     return true;
